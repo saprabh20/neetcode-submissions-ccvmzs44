de372286596045793277378db6003acaeb5e1024
Saprabh Anand <55506636+saprabh20@users.noreply.github.com>
2026-04-09 14:26:22 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> hm1 = new HashMap<>();
+        HashMap<Character, Integer> hm2 = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
+            if(hm1.containsKey(s.charAt(i))) {
+                int val = hm1.get(s.charAt(i));
+                hm1.put(s.charAt(i), ++val);
+            } else {
+                hm1.put(s.charAt(i),1);
+            }
+        }
+        for(int i = 0; i < t.length(); i++) {
+            if(hm2.containsKey(t.charAt(i))) {
+                int val = hm2.get(t.charAt(i));
+                hm2.put(t.charAt(i), ++val);
+            } else {
+                hm2.put(t.charAt(i),1);
+            }
+        }
+        if(hm1.equals(hm2)) {
+            return true;
+        }
+        return false;
+    }
+}
