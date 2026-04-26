5b8cb200a52e2563ff477b8fab55edc16da65111
Saprabh Anand <55506636+saprabh20@users.noreply.github.com>
2026-04-25 20:03:02 -0700

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null && list2 != null) {
+            return list2;
+        } else if(list1 != null && list2 == null) {
+            return list1;
+        } else if(list1 == null && list2 == null) {
+            return list1;
+        }
+        ListNode head = null;
+        ListNode ptr = null;
+        ListNode newNode = new ListNode();
+        head = newNode;
+        ptr = newNode;
+        if (list1.val <= list2.val) {
+            newNode.val = list1.val;
+            list1 = list1.next;
+        } else {
+            newNode.val = list2.val;
+            list2 = list2.next;
+        }
+        while (true) {
+            if(list1 == null || list2 == null) {
+                break;
+            }
+            ListNode node = new ListNode();
+            ptr.next = node;
+            ptr = node;
+            if (list1.val <= list2.val) {
+                node.val = list1.val;
+                list1 = list1.next;
+            } else {
+                node.val = list2.val;
+                list2 = list2.next;
+            }
+        }
+        while(list1 != null) {
+            ListNode node = new ListNode();
+            ptr.next = node;
+            ptr = node;
+            node.val = list1.val;
+            list1 = list1.next;
+        }
+        while(list2 != null) {
+            ListNode node = new ListNode();
+            ptr.next = node;
+            ptr = node;
+            node.val = list2.val;
+            list2 = list2.next;
+        }
+        return head;
+    }
+}
